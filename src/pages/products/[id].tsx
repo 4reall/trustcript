533e27ec6eca62190ee5d89b3eb4514b69e5c099
4reall <1l1b1v1f1@bk.ru>
2022-10-23 19:53:32 +0300

    hotfix

--- src/pages/products/[id].tsx
+++ src/pages/products/[id].tsx
@@ -1,9 +1,5 @@
 import { Typography } from 'src/common/layout/Typography.styles';
 
-import backBig from 'public/assets/images/notebook/notebook-back-big.png';
-import frontBig from 'public/assets/images/notebook/notebook-front-big.png';
-import leftBig from 'public/assets/images/notebook/notebook-left-big.png';
-import rightBig from 'public/assets/images/notebook/notebook-right-big.png';
 import { useRouter } from 'next/router';
 import { GetStaticPaths, GetStaticPropsContext } from 'next';
 import { IProductsParams } from 'src/lib/api/products/types/ProductsParams';
@@ -12,13 +8,6 @@ import {
 	GetProductParams,
 } from 'src/lib/api/products/services/getProduct/getProduct';
 import { getProducts } from 'src/lib/api/products/services/getProducts/getProducts';
-
-const images = [
-	{ src: backBig, alt: 'thumbnail' },
-	{ src: frontBig, alt: 'thumbnail' },
-	{ src: leftBig, alt: 'thumbnail' },
-	{ src: rightBig, alt: 'thumbnail' },
-];
 
 const ProductPage = () => {
 	// const { description, title } = getProductById(query.productId as string);
